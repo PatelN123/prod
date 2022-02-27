7a7bb4bea3dc6ac8e94aa0a24503b5d168e8fff8
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2022-02-27 15:56:28 +0000

    Update _middleware.js

--- platforms/pages/_middleware.js
+++ platforms/pages/_middleware.js
@@ -1,15 +1,21 @@
 import { NextResponse } from "next/server";
+import { minHeight } from "tailwindcss/defaultTheme";
 
 export default function middleware(req) {
   const url = req.nextUrl.clone(); // clone the request url
   const { pathname } = req.nextUrl; // get pathname of request (e.g. /blog-slug)
   const hostname = req.headers.get("host"); // get hostname of request (e.g. demo.vercel.pub)
 
+  // only for demo purposes – remove this if you want to use your root domain as the landing page
+  if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
+    return NextResponse.redirect("https://demo.vercel.pub");
+  }
+
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname
-          .replace(`.vercel.pub`, "") // you have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
-          .replace(`*.pages.notes.nayanpatel.net`, "") // you can use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
+          .replace(`.vercel.pub`, "")
+          .replace(`.pages.notes.nayanpatel.net`, "") // you can use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
       : // in this case, our team slug is "platformize", thus *.platformize.vercel.app works
         hostname.replace(`.localhost:3000`, "");
 
@@ -30,7 +36,7 @@ export default function middleware(req) {
       return NextResponse.rewrite(url);
     } else if (
       hostname === "localhost:3000" ||
-      hostname === "pages.notes.nayanpatel.net"
+      hostname === "platformize.vercel.app"
     ) {
       url.pathname = `/home`;
       return NextResponse.rewrite(url);
@@ -39,4 +45,3 @@ export default function middleware(req) {
       return NextResponse.rewrite(url);
     }
   }
-}
