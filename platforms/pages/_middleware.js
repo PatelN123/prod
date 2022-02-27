ffa68a32a0a21da01900526909c8d52a1db99ebf
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2022-02-27 15:49:28 +0000

    Update _middleware.js

--- platforms/pages/_middleware.js
+++ platforms/pages/_middleware.js
@@ -3,18 +3,13 @@ import { NextResponse } from "next/server";
 export default function middleware(req) {
   const url = req.nextUrl.clone(); // clone the request url
   const { pathname } = req.nextUrl; // get pathname of request (e.g. /blog-slug)
-  const hostname = req.headers.get("host"); // get hostname of request (e.g. dsn-pages-go.vercel.app)
-
-  // only for demo purposes – remove this if you want to use your root domain as the landing page
-  if (hostname === "dsn-pages-go.vercel.app" || hostname === "platforms.vercel.app") {
-    return NextResponse.redirect("https://dsn-pages-go.vercel.app");
-  }
+  const hostname = req.headers.get("host"); // get hostname of request (e.g. demo.vercel.pub)
 
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname
-          .replace(`.vercel.app`, "") // you have to replace ".dsn-pages-go.vercel.app" with your own domain if you deploy this example under your domain.
-          .replace(`.digital-support-notes.vercel.app`, "") // you can use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
+          .replace(`.pages.notes.nayanpatel.net`, "") // you have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
+          .replace(`*.pages.notes.nayanpatel.net`, "") // you can use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
       : // in this case, our team slug is "platformize", thus *.platformize.vercel.app works
         hostname.replace(`.localhost:3000`, "");
 
@@ -35,7 +30,7 @@ export default function middleware(req) {
       return NextResponse.rewrite(url);
     } else if (
       hostname === "localhost:3000" ||
-      hostname === "platformize.vercel.app"
+      hostname === "pages.notes.nayanpatel.net"
     ) {
       url.pathname = `/home`;
       return NextResponse.rewrite(url);
